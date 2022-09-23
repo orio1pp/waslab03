a773d47b9400c2fb8f2f1d068900033c9e075d01
Alejandro Lozano Aparicio <alex2001lozano@gmail.com>
2022-09-23 09:30:20 +0200

    tasca #5 finalitzada

--- WASLab03_SimpleTwitterClient/src/twitter/SimpleClient.java
+++ WASLab03_SimpleTwitterClient/src/twitter/SimpleClient.java
@@ -1,7 +1,11 @@
 package twitter;
 
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.util.Date;
 
+import twitter4j.JSONObject;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
@@ -9,12 +13,15 @@ import twitter4j.TwitterFactory;
 public class SimpleClient {
 
 	public static void main(String[] args) throws Exception {
-		
 		final Twitter twitter = new TwitterFactory().getInstance();
+		long tweetID = twitter.getUserTimeline("fib_was").get(0).getId();
+		System.out.println(tweetID);
+		twitter.retweetStatus(tweetID);
 		
+		/*
 		Date now = new Date();
 		String latestStatus = "Hey @fib_was, we've just completed task #4 [timestamp: "+now+"]";
 		Status status = twitter.updateStatus(latestStatus);
-		System.out.println("Successfully updated the status to: " + status.getText());       
+		System.out.println("Successfully updated the status to: " + status.getText());*/  
 	}
 }
