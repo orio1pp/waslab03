0448687260073d727f4623ea4987175be7105bd9
orio1pp <oriol.portell@estudiantat.upc.edu>
2022-09-29 20:22:40 +0200

    Tasca 6 finalitzada

--- WASLab03_SimpleTwitterClient/src/twitter/SimpleClient.java
+++ WASLab03_SimpleTwitterClient/src/twitter/SimpleClient.java
@@ -1,53 +1,63 @@
 package twitter;
-import java.util.List;
 
+
+import java.util.List;
+import java.util.Map;
+
+import twitter4j.GeoLocation;
 import twitter4j.Query;
+import twitter4j.Query.Unit;
 import twitter4j.QueryResult;
+import twitter4j.RateLimitStatus;
 import twitter4j.Status;
 import twitter4j.Trend;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 
 public class SimpleClient {
- 	public static boolean checkIsInBarcelona(String name) {
- 		return name.equals("Barcelona") || name.equals("Barcelona, España") ||
- 				name.equals("Barcelona, Cataluña");
- 	}
-	 public static void main(String[] args) throws Exception {
 
-	        final Twitter twitter = new TwitterFactory().getInstance();
-	        int bcnCode = 753692;
+    public static void main(String[] args) throws Exception {
+        final Twitter twitter = new TwitterFactory().getInstance();
+        int bcnCode = 753692;
+       
+        /* get top 10 trending topics in bcn */
+        Trend[] trendsBCN = twitter.getPlaceTrends(bcnCode).getTrends();
+        int maxLen = trendsBCN.length;
+        if (maxLen > 10) maxLen = 10;
+        //for (int timer = 0; timer < 10000; ++timer);
+		double lat = 41.3870154;
+		double lon = 2.1700471;
+		GeoLocation geolocation = new GeoLocation(lat, lon);
+		Unit units = Query.KILOMETERS;
+		Query catTrends;
+        for (int i = 0; i < maxLen; ++i) {
+            System.out.println("Trending topic: "+ trendsBCN[i].getName());
+    		catTrends = new Query( trendsBCN[i].getName());
+    		catTrends.setGeoCode(geolocation, 5.0, units);
+    		catTrends.setResultType(Query.RECENT);
+            QueryResult query = twitter.search(catTrends);
 
-	        /* get 10 1st trend top in bcn */
-	        for(int timer=0; timer<100000000; timer++);
-	        Trend[] trendsBCN = twitter.getPlaceTrends(bcnCode).getTrends();
-	        for(int timer=0; timer<100000000; timer++);
-	        for (int i = 0; i < 10; ++i) {
-	        	//System.out.println(trendsBCN[i]);
-	        	QueryResult tweets = twitter.search(new Query (trendsBCN[i].getName()));
-	        	for(int timer=0; timer<100000000; timer++);
-	        	int numFound = 0;
-	        	boolean found = false;
-	        	while(tweets.hasNext() && !found) {
-	            	List<Status> status = tweets.getTweets();
-	            	for(int j = 0; j<15 && !found; j++) {
-	            		Status tweet = status.get(j);
-	            		String nameCity = status.get(j).getUser().getLocation();
-	            		//System.out.println(nameCity);
-	            		if(checkIsInBarcelona(nameCity)) {
-	                        System.out.println("    - " + tweet.getUser().getName() + " (@" + 
-	                                tweet.getUser().getScreenName() + "): " + 
-	                                tweet.getText());
-	            			numFound++;
-		            		if(numFound == 5) found = true;
-	            		}
-	            	}
-	            	
-	            	//System.out.println(i + " " + trendsBCN[i].getName() + trendsBCN[i].toString());
-	            }
-	        	found = false;
-	        	numFound = 0;
-	        }
-	      }
+            int count = 0;
+            boolean found = false;
 
+            while (query.hasNext() && !found) {
+                List<Status> tweets = query.getTweets();
+                for (int j = 0; j < tweets.size() && count < 5; ++j) {
+                    Status tweet = tweets.get(j);
+                    if (true /* pl cat */) {
+                        System.out.println("    - " + tweet.getUser().getName() + " (@" + 
+                                tweet.getUser().getScreenName() + "): " + 
+                                tweet.getText());
+                        count++;
+                    }
+                }
+                if (count == 5) {
+                    count = 0;
+                    found = true;
+                }
+                query = twitter.search(query.nextQuery());
+            }
+        }
+    }
+ 
 }
